2d7cbeb9651c39763400f3d48d5a332c40a38b6f
Nicolas Lepage <19571875+nlepage@users.noreply.github.com>
2023-06-19 23:46:16 +0200

    Add Press Enter message and handler

--- title/title.go
+++ title/title.go
@@ -5,6 +5,7 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 	"github.com/hajimehoshi/ebiten/v2/text"
 	"golang.org/x/image/font"
 	"golang.org/x/image/font/opentype"
@@ -17,15 +18,21 @@ type Scene struct {
 	g    *game.Game
 	Next game.Scene
 
-	titleFont   font.Face
-	titleColor  color.Color
-	titleBounds image.Rectangle
+	textColor color.Color
+
+	titleFace font.Face
+	titlePos  image.Point
+
+	startFace font.Face
+	startPos  image.Point
 }
 
 var _ game.Scene = &Scene{}
 
 func (s *Scene) Init(game *game.Game) error {
 	s.g = game
+
+	s.textColor = color.RGBA{84, 137, 169, 0}
 
 	face, err := assets.SrirachaRegular(&opentype.FaceOptions{
 		DPI:  72,
@@ -34,11 +41,20 @@ func (s *Scene) Init(game *game.Game) error {
 	if err != nil {
 		return err
 	}
-	s.titleFont = face
+	s.titleFace = face
+	titleBounds := text.BoundString(s.titleFace, "Psyché")
+	s.titlePos.X, s.titlePos.Y = (1920-titleBounds.Dx())/2, 400
 
-	s.titleColor = color.RGBA{84, 137, 169, 0}
-
-	s.titleBounds = text.BoundString(s.titleFont, "Psyché")
+	face, err = assets.SrirachaRegular(&opentype.FaceOptions{
+		DPI:  48,
+		Size: 120,
+	})
+	if err != nil {
+		return err
+	}
+	s.startFace = face
+	startBounds := text.BoundString(s.startFace, "Press Enter to start")
+	s.startPos.X, s.startPos.Y = (1920-startBounds.Dx())/2, 600
 
 	return nil
 }
@@ -46,15 +62,19 @@ func (s *Scene) Init(game *game.Game) error {
 func (s *Scene) Draw(screen *ebiten.Image) {
 	screen.Fill(color.Black)
 
-	text.Draw(screen, "Psyché", s.titleFont, (1920-s.titleBounds.Dx())/2, (1080-s.titleBounds.Dy())/2, s.titleColor)
+	text.Draw(screen, "Psyché", s.titleFace, s.titlePos.X, s.titlePos.Y, s.textColor)
+	text.Draw(screen, "Press Enter to start", s.startFace, s.startPos.X, s.startPos.Y, s.textColor)
 }
 
 func (s *Scene) Update() error {
+	if inpututil.IsKeyJustPressed(ebiten.KeyEnter) {
+		return s.g.SetScene(s.Next)
+	}
+
 	return nil
 }
 
 func (s *Scene) Dispose() {
-
 }
 
 func (s *Scene) Layout(_, _ int) (int, int) {
